7f534f29bfc636a1fe74a83f21627e07704e709d
Kei-K23 <keiksl2301@gmail.com>
2024-11-08 22:14:40 +0630

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Memory Game 🐱🐼
 
 A classic card matching memory game that build with **Vue**
+
+Live demo 🌐: [https://vue-memory-game-ten.vercel.app](https://vue-memory-game-ten.vercel.app/)
+
+## Tech Stack
+
+- Vue 3
+- TypeScript
+- TailwindCSS
+
+#### Desktop View
+
+![Desktop View](/public/p1.png)
+
+#### Mobile View
+
+![Mobile View](/public/p2.png)
--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,17 @@ import GameBoard from "./components/GameBoard.vue";
 
 <template>
   <main
-    class="min-h-screen w-full bg-neutral-900 flex flex-col items-center justify-center mx-auto text-neutral-100 px-4"
+    class="min-h-screen w-full bg-neutral-900 flex flex-col items-center justify-center mx-auto text-neutral-100 px-4 relative"
   >
+    <div class="absolute top-2 right-4 md:top-4 md:right-6">
+      <a href="">
+        <img
+          src="/github_icon.svg"
+          alt="github_icon"
+          class="size-5 md:size-7"
+        />
+      </a>
+    </div>
     <div
       class="h-full flex-1 flex flex-col items-center justify-start md:justify-center container mx-auto w-full mt-10 md:mt-0"
     >
